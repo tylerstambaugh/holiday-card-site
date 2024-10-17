1964c40c8c43bcfbac6a6601fcbb6b324de2b5c2
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-10-16 21:08:15 -0400

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 
+import { Outlet, Route,  BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import { Home } from './pages/home'
+import { Header } from './components/header'
+import Footer from './components/footer'
 
-function App() {
-
+function Layout() {
   return (
     <>
-      <h1>Tyler's Holiday Cards</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <Header />
+      <Outlet /> {/* This will render the nested child route */}
+      <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Define a layout route to include Header and Footer */}
+        <Route path="/" element={<Layout />}>
+          {/* Home component will be rendered in the Outlet */}
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
