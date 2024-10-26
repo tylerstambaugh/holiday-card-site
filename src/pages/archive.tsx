ed4f75d4258a7f186023d7b3d08e1a3e43ab288b
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-10-26 07:38:00 -0400

    setting up photo library

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,40 +1,50 @@
-import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material"
-import React from "react"
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button } from "@mui/material"
+import React, { useState } from "react"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Front2020 from '../assets/images/holiday-cards/2020_Corona.jpg';
+import Back2020 from '../assets/images/holiday-cards/2020_Corona_back.jpg';
+import Front2021 from '../assets/images/holiday-cards/2021_Bobeis_And_BooneHound.jpg';
+import Back2021 from '../assets/images/holiday-cards/2021_Bobeis_And_BooneHound_back.jpg';
+import Front2022 from '../assets/images/holiday-cards/2022_CreationOfBoone.jpg';
+import Back2022 from '../assets/images/holiday-cards/2022_CreationOfBoone_back.jpg';
+import Front2023 from '../assets/images/holiday-cards/2023_American_Gothic.jpg';
+import Back2023 from '../assets/images/holiday-cards/2023_American_Gothic_back.jpg';
 
 
 type ArchiveItem = {
     photoFrontPath: string;
     photoBackPath: string;
     year: number;
+    showBack: boolean;
 }
 export const Archive: React.FC = () => {
 
+    const [showPhotoBack, setShowPhotoBack] = useState<{ showBack: boolean, index: number }[]>([{ showBack: false, index: 1 }]);
+
     const archiveItems: ArchiveItem[] = [
         {
-            photoFrontPath: "asdf",
-            photoBackPath: "asdf",
-            year: 2020
+            photoFrontPath: Front2020,
+            photoBackPath: Back2020,
+            year: 2020,
+            showBack: false
         },
         {
-            photoFrontPath: "asdf",
-            photoBackPath: "asdf",
-            year: 2020
+            photoFrontPath: Front2021,
+            photoBackPath: Back2021,
+            year: 2021,
+            showBack: false
         },
         {
-            photoFrontPath: "asdf",
-            photoBackPath: "asdf",
-            year: 2020
+            photoFrontPath: Front2022,
+            photoBackPath: Back2022,
+            year: 2022,
+            showBack: false
         },
         {
-            photoFrontPath: "asdf",
-            photoBackPath: "asdf",
-            year: 2020
-        },
-        {
-            photoFrontPath: "asdf",
-            photoBackPath: "asdf",
-            year: 2020
+            photoFrontPath: Front2023,
+            photoBackPath: Back2023,
+            year: 2023,
+            showBack: false
         },
     ]
 
@@ -42,48 +52,42 @@ export const Archive: React.FC = () => {
         <div>
 
             {archiveItems.map((item: ArchiveItem, index) => (
-
-            <Accordion key={index}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1-content"
-                    id={`panel${index}-header`}
-                >
-                   {item.year}
-                </AccordionSummary>
-                <AccordionDetails>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                </AccordionDetails>
-            </Accordion>
+                //need to populate the setShowPhotoBack state when creating the items. 
+                <Accordion key={index}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1-content"
+                        id={`panel${index}-header`}
+                    >
+                        {item.year}
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Box className={"arcive-photo-box"}>
+                            {showPhotoBack.find((i) => i.index === index)?.showBack ? (
+                                <img src={item.photoBackPath} className="arcive-photo-box" />
+                                ) : (
+                                    <img src={item.photoFrontPath} className="arcive-photo-box" />
+                                )
+                            }
+                        </Box>
+                        <Button onClick={() => {
+                            // Create a new state object with the updated showBack value
+                            const updatedShowPhotoBack = showPhotoBack.map((item) => {
+                                if (item.index + 1 === index) {
+                                    return { ...item, showBack: !item.showBack }; // Toggle showBack for the clicked item
+                                }
+                                return item; // Keep existing values for other items
+                            });
+                            console.log("updated show back", updatedShowPhotoBack);
+                            
+                            setShowPhotoBack(updatedShowPhotoBack);
+                        }}>
+                            {showPhotoBack.find((i) => i.index + 1 === index)?.showBack ? "Show Front" : "Show Back"}
+                        </Button>
+                    </AccordionDetails>
+                </Accordion>
             )
             )}
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1-content"
-                    id="panel1-header"
-                >
-                    Accordion 1
-                </AccordionSummary>
-                <AccordionDetails>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                </AccordionDetails>
-            </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2-content"
-                    id="panel2-header"
-                >
-                    Accordion 2
-                </AccordionSummary>
-                <AccordionDetails>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                </AccordionDetails>
-            </Accordion>
 
         </div>
     )
