2f86d983cb9f8b51ac0618281f81a339380f1a34
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2024-10-30 21:30:48 -0400

    more playing with styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import "../App.css"
 import { useNavigate } from "react-router-dom";
 
-const pages = [{name: 'Home', route:'/'}, {name: 'Archive', route:'/archive'}];
+const pages = [{ name: 'Home', route: '/' }, { name: 'Archive', route: '/archive' }];
 
 
 export const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -20,84 +20,84 @@ export const Header = () => {
 
   return (
 
-        <Toolbar  className="header">
+    <Toolbar className="header">
       <Link variant="h6" underline="none" color="inherit" href="/">
-          <Typography
-            variant="h6"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Tyler's Holiday Card Emporium
-          </Typography>
-              </Link>
+        <Typography
+          variant="h6"
+          noWrap
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          Tyler's Holiday Cards
+        </Typography>
+      </Link>
 
-          <Box sx={{ flexGrow: 1, display: { md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page.name} onClick={() => {navigate(`${page.route}`); handleCloseNavMenu()}}>
-                  <Typography sx={{ textAlign: 'center' }}>{page.name}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+      <Box sx={{  display: { md: 'none' } }}>
+        <IconButton
+          size="large"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpenNavMenu}
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{ display: { xs: 'block', md: 'none' } }}
+        >
+          {pages.map((page) => (
+            <MenuItem key={page.name} onClick={() => { navigate(`${page.route}`); handleCloseNavMenu() }}>
+              <Typography sx={{ textAlign: 'center' }}>{page.name}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
       <Link variant="h6" underline="none" color="inherit" href="/" >
-          <Typography
-            variant="h6"   
-            sx={{
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
+        <Typography
+          variant="h6"
+          sx={{
+            display: { xs: 'flex', md: 'none' },
+            flexGrow: 1,
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          Tyler's Holiday Cards
+        </Typography>
+      </Link>
+      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        {pages.map((page) => (
+          <Button
+            key={page.name}
+            onClick={() => { navigate(`${page.route}`); handleCloseNavMenu() }}
+            sx={{ my: 2, color: 'white', display: 'block' }}
           >
-             Tyler's Holiday Cards
-          </Typography>
-          </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page.name}
-                onClick={() => {navigate(`${page.route}`); handleCloseNavMenu()}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page.name}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
+            {page.name}
+          </Button>
+        ))}
+      </Box>
+    </Toolbar>
   );
 }
