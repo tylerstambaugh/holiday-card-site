68b46b647d6958bf399d6a24455928c9ab7a33dc
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2025-11-19 19:48:36 -0500

    2025 updates

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -29,10 +29,13 @@ export const Home: React.FC = () => {
                                     2025 Year In Review
                                 </Typography>
                                 <Typography>
-                                    Happy holidays from our family to yours! As we wrap up another year, we wanted to take a moment to reflect on the highlights and memorable moments that made 2025 special for us.
-                                    The biggest news to report on is that Tyler moved to Bloomington to be closer to Cindy. Whiles Indianapolis has its charm, being able to spend more time together has been the best gift of all. Additionally, since we spend most of our free time riding bicycles, it made sense to be closer to some of the best routes in the state. 
-                                    The adjustment was really smooth given Bloomington is where we were both born and are familiar with the town. We spent a good part of the year doing some renovations and have turned the house into a home.
-                                     Boone has adapted well to the new environment, he loves going for walks around campus. We hope you all are doing well too and look forward to hearing from you soon.
+                                    Happy holidays from our family to yours! As we wrap up another year, we wanted to share little bit about what we've been up to.
+                                    The biggest news to report on is that Tyler moved to Bloomington to be closer to Cindy. While Indianapolis was fine for the last 20 years, with his new job being fully remote there was nothing keeping himm there. 
+                                    Being able to spend more time together has been the best gift of all. 
+                                    Additionally, since we spend most of our free time riding bicycles, it made sense to be closer to some of the best routes in the state. 
+                                    The adjustment was really smooth given Bloomington is where we were both born and that we are familiar with the town. We spent a good part of the year doing some renovations and have turned the house into a home.
+                                    Ol Boone has adapted well to the new environment, we frequently take him for walks around the Indiana University campus and he's made some friends at the walk-up ice cream stand where they love serving him pup cups.
+                                    We hope you all are doing well too and look forward to hearing from you soon.
                                 </Typography>
                             </Box>
                             <Stack justifyContent="center" spacing={2}>
